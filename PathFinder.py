0602a8ef8957285d008e3bd3de312d28b0ae7d01
Rebecca Svensson <rebsven@student.chalmers.se>
2019-03-06 17:29:47 +0100

    Ett par hjälpfunktioner tillagda

--- PathFinder.py
+++ PathFinder.py
@@ -1,1 +1,31 @@
 # Här kan vi lägga algoritmen och saker relaterade till den
+import Maze
+
+
+class HelpFunctions:
+
+    def currentCell(self, maze):
+        return maze.x, maze.y
+
+    # lastDirection är sträng (NSWE), t.ex. N
+    def updateCurrentCell(self, maze, lastDirection):
+        if lastDirection == 'N':
+            maze.y = maze.y + 1
+        elif lastDirection == 'S':
+            maze.y = maze.y - 1
+        elif lastDirection == 'W':
+            maze.x = maze.x - 1
+        elif lastDirection == 'E':
+            maze.x = maze.x + 1
+        else:
+            None
+
+
+
+
+
+
+
+
+
+
