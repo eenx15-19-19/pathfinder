5a6693a8117a01f338a95b793a732f6714f50d00
Rebecca Svensson <rebsven@student.chalmers.se>
2019-03-26 17:06:30 +0100

    Omvandling av labyrint

---
+++ MazeTransformer.py
@@ -0,0 +1,13 @@
+import binascii
+
+
+class MazeTransformer:
+
+    hexString = '0x01'
+    hexNumber = int(hexString.lstrip('0x'))
+    print(type(hexNumber))
+    number = hexNumber
+    bin_number = bin(number).lstrip('0b').zfill(4)
+    print(bin_number)
+
+    # NESW gör om till NSWE
