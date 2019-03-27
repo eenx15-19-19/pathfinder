3e2d7a5236d2856b56236b382e5bad67f4c543a6
Emil Grimheden <emil.grimheden@gmail.com>
2019-03-28 00:48:52 +0100

    Funkar inte

--- MazeTransformer.py
+++ MazeTransformer.py
@@ -1,17 +1,30 @@
 from Translation import Translation
+import numpy as np
 
 class MazeTransformer:
+    translator = Translation()
     f = open("5x5.c", "r")
     whole = f.read()
     hextype = whole[-1577:-26]
-    matrixh = []
-    matrixb = []
+    listh = []
+    listb = []
     for i in range(256):
-        matrixh.append(int(hextype[hextype.index(",", 1 + i * 6) - 4:hextype.index(",", 1 + i * 6)], 16))
-    for element in matrixh:
-        matrixb.append(translator.change_maze_format(f'{element:0>4b}'))
-    print(matrixb)
+        listh.append(int(hextype[hextype.index(",", 1 + i * 6) - 4:hextype.index(",", 1 + i * 6)], 16))
+    for element in listh:
+        listb.append(translator.change_maze_format(f'{element:0>4b}'))
+    print(listb[1][1])
+    matrixb = np.empty([16, 16], dtype="S4")
 
+    #print(matrixb[0][15])
+
+    for x in range(15):
+        for y in range(15, 0, -1):
+            for k in listb[x*15+(15-y)]:
+
+                element = listb[x*15+(15-y)]
+                #print(element)
+                #matrixb[y, x] = element
+    #print(matrixb)
 
 
     #hexString = '0x01'
