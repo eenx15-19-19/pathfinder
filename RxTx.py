d0165fb653faf34844880769b1fe531ae576fcdd
Isak Aslund <aslundisak@gmail.com>
2019-05-07 13:59:23 +0100

    Bug fixes for communication

--- RxTx.py
+++ RxTx.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import serial
+import io
 ser = serial.Serial(
     port='/dev/ttyS0',
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    timeout=0.1
+    timeout=0.5
 
 )
 import Robot
@@ -15,18 +16,20 @@ import Maze
 from Main import Main
 
 def main():
+    print("Running Main!")
     finder = PathFinder()
 
     run_instruction = None
 
-    maze = Maze.Maze(13, 6)
-    robot = Robot.Robot(maze)
-
-    while run_instruction.equals(None):
-        run_instruction = ser.read(4)
-
-        if run_instruction=='fas1':
-            sLast=1
+    maze = None
+    robot = None
+    
+    while run_instruction == None: 
+        run_instruction = ser.readline()
+        print("Run Instruction: " + str(run_instruction))
+        if run_instruction == b'fas1\n':
+            print("--------- FAS1 ----------")
+            sLast=0
 
             # Initiera maze och robot
             maze = Maze.Maze(13, 6)
@@ -40,25 +43,30 @@ def main():
 
             if run_instruction == 'fas2':
                 run_instruction = fas2(maze, robot)
-
-        if run_instruction == 'fas3':
+        elif run_instruction == 'fas3':
             run_instruction = fas3(maze, robot)
+        else:
+            run_instruction = None
 
 
 def fas1(sLast, maze, robot):
     while not maze.win:
-        cell=ser.read(5)
-        if cell == 'e':
-            return None
-        while sLast == cell[0]: #Check if package is replica of the one before.
-            ser.write('wrong package')
-            cell=ser.read(5)
-        sLast = cell[0]
+        cell=ser.readline()
+        print("Cell: " + str(cell))
+        if cell == b'e':
+            print(" ------------    ERROR fas1() -----------")
+            # return None
+        #while sLast == b+cell[0]:#Check if package is replica of the one before.
+         #   print("wrong package")
+          #  cell=ser.readline()
+        #sLast = cell[0]
         cell = cell[1:]   #remove the sequence number
-
+        print(" ------------------ CELL IS OF TYPE ------------")
+        print(type(cell))
+        print(type(str(cell)))
         main_class = Main()
         direction = main_class.run(maze, robot, cell)
-
+        print("Sending command: " + direction)
         ser.write(direction)
 
         if robot.current_pos_row == maze.end_row and robot.current_pos_col == maze.end_col:
